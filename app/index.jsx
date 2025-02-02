8500ed79db2c7df430d843bbfe0c4305d9659f36
James Sadler <28812304+sadlerjames@users.noreply.github.com>
2025-02-02 13:47:39 +0000

    Added and configured Poppins font

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl">VascTrack</Text>
+      <Text className="text-4xl font-pbold">VascTrack App</Text>
       <StatusBar style="auto" />
       <Link href="/profile">Go to Profile!</Link>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,30 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#D1F0FF',
+        secondary: '#ADE2FF',
+        tertiary: '#1F8FFF',
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
