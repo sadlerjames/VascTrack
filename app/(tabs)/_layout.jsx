024e3fa3ed8d915c807ee629af264f83c3c1512c
James Sadler <28812304+sadlerjames@users.noreply.github.com>
2025-02-09 17:31:00 +0000

    Added Tab Navigation Bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,17 +1,108 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs, Redirect } from 'expo-router'
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { Tabs, Redirect } from 'expo-router';
+
+import icons from '../../constants/icons';
+
+const TabIcon = ({icon, color, name, focused}) => {
+  return(
+    <View className="items-center justify-center w-full py-1">
+      <Image 
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6 mb-1"
+      />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs text-center`} adjustsFontSizeToFit={true} minimumFontScale={0.8} >
+        {name}
+      </Text>
+    </View>
+  ) 
+}
 
 const TabsLayout = () => {
   return (
-    <>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#ADE2FF',
+            borderTopWidth: 1,
+            height: 70,
+            paddingBottom: 8,
+            paddingHorizontal: 5
+          },
+          tabBarItemStyle: {
+            paddingVertical: 5,
+            flex: 1,
+          }
+        }}
+      >
         <Tabs.Screen
-        
+          name="home"
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="insights"
+          options={{
+            title: 'Insights',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.graph}
+                color={color}
+                name="Insights"
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="record"
+          options={{
+            title: 'Record',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name="Record"
+                focused={focused}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            )
+          }}
         />
       </Tabs>
-    </>
-  )
+  );
 }
 
 export default TabsLayout
---
+++ constants/icons.js
@@ -0,0 +1,11 @@
+import graph from "../assets/icons/Graph.png";
+import home from "../assets/icons/Home.png";
+import plus from "../assets/icons/Plus.png";
+import profile from "../assets/icons/Profile.png";
+
+export default {
+    graph,
+    home,
+    plus,
+    profile
+}
