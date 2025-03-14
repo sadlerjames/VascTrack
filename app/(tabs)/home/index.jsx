b5a434cc7b0f0a0ae2509495f5f247b9700d46f9
James Sadler <28812304+sadlerjames@users.noreply.github.com>
2025-03-14 11:13:04 +0000

    Updated cards on homescreen

--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -1,26 +1,66 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Alert } from 'react-native'
 import { router } from 'expo-router';
-import React from 'react'
+import React, { useState } from 'react'
 
 import ScreenWrapper from '../../../components/ScreenWrapper'
 import CustomButton from '../../../components/CustomButton'
+import ProgressBar from '../../../components/ProgressBar';
+import { useGlobalContext } from '../../../context/GlobalProvider';
+import { recordEnergy } from '../../../lib/recording';
 import icons from '../../../constants/icons';
 
 const Home = () => {
+  const { user } = useGlobalContext();
+  
+    const [barSelected, setBarSelected] = useState(0);
+
+    // Submit to DB to store
+    const submit = async () => {
+      try {
+          if (!user) throw new Error("User not logged in");
+          if (!barSelected) {
+              Alert.alert("Error", "Please choose an energy level");
+              return;
+          }
+  
+          // Call Firestore function to save data
+          await recordEnergy(user.uid, barSelected-1);
+  
+          Alert.alert("Success", "Energy recorded successfully!"); 
+      } catch (error) {
+          Alert.alert("Error", error.message);
+      }
+    };
   return (
     <ScreenWrapper>
     
 
       <View className="flex-1 items-center justify-center">
 
+      <View className="w-11/12 bg-primary p-6 rounded-2xl shadow-sm shadow-primary mb-4">
+          <Text className="text-2xl font-pbold text-center">How much energy do you have?</Text>
+          <ProgressBar selected={barSelected} setSelected={setBarSelected} containerStyle="pt-5 pb-3" />
+          
+          <View className="items-center">
+            <CustomButton
+              title="Record"
+              handlePress={submit}
+              containerStyles="w-80 bg-tertiary mt-3"
+              textStyles="text-2xl"
+            />  
+          </View>
+
+        </View>
+
         {/* Row of Two Cards */}
         <View className="flex-row w-11/12 justify-between">
           
           {/* First Card */}
           <TouchableOpacity 
-            className="flex-1 bg-primary p-4 rounded-2xl shadow-sm shadow-primary"
+            className="w-[48%] bg-primary p-4 rounded-2xl shadow-sm shadow-primary"
             onPress={() => router.push('/home/chatBot')} 
           >
+            <Text className="text-2xl font-pbold text-center">Have a chat!</Text>
             <Image 
               source={icons.robot}
               className="w-full h-32"
@@ -29,12 +69,12 @@ const Home = () => {
           </TouchableOpacity>
 
           {/* Second Card */}
-          <View className="flex-1 bg-primary p-4 rounded-2xl shadow-sm shadow-primary">
-            <Image 
-              source={icons.graph}
-              className="w-full h-32"
-              resizeMode='contain'
-            />
+          <View className="w-[48%] bg-primary p-4 rounded-2xl shadow-sm shadow-primary">
+            <Text className="text-xl font-psemibold text-center">Next dosage in</Text>
+            <View className="flex-1 justify-center items-center">
+              <Text className="text-4xl font-pbold text-center">4 hours</Text>
+              <Text className="text-4xl font-pbold text-center">3 mins</Text>
+            </View>
           </View>
 
         </View>
