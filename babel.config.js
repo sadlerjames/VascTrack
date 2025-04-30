f92112314222f341c2ff0d9c738465c6306043e1
James Sadler <28812304+sadlerjames@users.noreply.github.com>
2025-04-30 21:04:12 +0100

    Fixed android bundle configuration

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-      "module:metro-react-native-babel-preset",
-      "@babel/preset-env",
-    ],
-    plugins: [
-      ["module:react-native-dotenv"],
-    ],
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel",],
+    plugins: [['module:react-native-dotenv']],
   };
 };
